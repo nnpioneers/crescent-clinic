342050f9a6cee66bb1fa1876e3d0c94b5ab40db3
ncoders06-hue <ncoders06@gmail.com>
2026-07-05 12:18:19 +0530

    fix: mute display errors to resolve white screen corruption

--- api/api.php
+++ api/api.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+error_reporting(0);
+ini_set('display_errors', 0);
 date_default_timezone_set('Asia/Kolkata');
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
 header('Cache-Control: post-check=0, pre-check=0', false);
