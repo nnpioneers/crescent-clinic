0e563f9775be3e6c7b739174e07f65124c4c704a
ncoders06-hue <ncoders06@gmail.com>
2026-07-13 13:04:01 +0530

    diagnostics: remove temporary opcache reset script

--- debug_deploy.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-echo "OPcache Status:\n";
-if (function_exists('opcache_reset')) {
-    echo "Resetting: " . (opcache_reset() ? "SUCCESS" : "FAILED") . "\n";
-} else {
-    echo "opcache_reset() is not available.\n";
-}
